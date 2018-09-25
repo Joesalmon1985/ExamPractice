3f73936fffe953c47049d3c55ccf126cf97be425
Joesalmon1985 <Joesalmon1985@gmail.com>
2018-09-25 21:26:21 +0100

    Create PersonTest.java

---
+++ PersonTest.java
@@ -0,0 +1,94 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class PersonTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class PersonTest
+{
+    /**
+     * Default constructor for test class PersonTest
+     */
+    public PersonTest()
+    {
+        Person fred_smith, mary_jones;
+        fred_smith = new Person ("Fred", "Smith");
+        mary_jones = new Person ("Jones");
+    }
+
+    
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void Person()
+    {
+    }
+
+    @Test
+    public void getInitals()
+    {
+        String testInitalsSS, testInitalsJ;
+        Person sarah_smith, susan_jones;
+        sarah_smith = new Person ("Sarah", "Smith");
+        susan_jones = new Person ("Jones");
+        testInitalsSS = sarah_smith.getInitals();
+        testInitalsJ = susan_jones.getInitals();
+        assertEquals("SS", testInitalsSS);
+        assertEquals("J", testInitalsJ);
+       
+    }
+    
+    @Test
+    public void getName()
+    {
+        String testNameFS, testNameMJ;
+        Person fred_smith, mary_jones;
+        fred_smith = new Person ("Fred", "Smith");
+        mary_jones = new Person ("Jones");
+        testNameFS = fred_smith.getName();
+        testNameMJ = mary_jones.getName();
+        assertEquals("Fred Smith", testNameFS);
+        assertEquals("Jones", testNameMJ);
+    }
+        
+    @Test
+    public void printReverse()
+    {
+        String testReverseSS, testReverseJ;
+        Person sarah_smith, susan_jones;
+        sarah_smith = new Person ("Sarah", "Smith");
+        susan_jones = new Person ("Jones");
+        sarah_smith.printReverse();
+        // Not sure how to test this to be honest.
+    }
+        
+}
+
+
